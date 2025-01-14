957d496e1e478d683570b71b1887c01f75f00021
Caglar Özdemir <c.oezdemir@oeeng.de>
2025-01-14 12:45:58 +0100

    add Db delete user test

--- DbService.Tests/DbServiceTests.cs
+++ DbService.Tests/DbServiceTests.cs
@@ -1,38 +1,65 @@
 ﻿using DbService.Common.Contexts;
 using DbService.Common.Dtos;
+using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 
 namespace DbService.Tests;
 
 public class DbServiceTests: IDisposable
 {
+    private readonly UserDbContext _context;
+    private readonly UserDto _user;
+    public DbServiceTests()
+    {
+        var options = new DbContextOptionsBuilder<UserDbContext>()
+        .UseInMemoryDatabase(databaseName: "TestDatabase")
+        .Options;
+
+        _context = new(options);
+
+        _user = new()
+        {
+            FirstName = "Caglar",
+            FamilyName = "Oezdemir",
+            Email = "info@oeeng.de"
+        };
+    }
+
     public void Dispose()
     {
-
+        _context.Dispose();
     }
 
     [Fact]
-    public void Test1()
+    public void AddUser_UserDto_CorrectUserDto()
     {
         // Arrange 
-        var options = new DbContextOptionsBuilder<UserDbContext>()
-            .UseInMemoryDatabase(databaseName: "TestDatabase")
-            .Options;
-
-        var context = new UserDbContext(options);
-
-        var dbService = new DbService(context);
+        var dbService = new DbService(_context);
 
         // Act
-        dbService.AddUser(new UserDto { Id = 1, FirstName = "Caglar",FamilyName="Oezdemir",Email="info@oeeng.de" });
+        dbService.AddUser(_user);
 
 
         // Assert
-        var user = context.Users.First();
-
-
-       
-
-
+        var user = _context.Users.First();      
+        user.Should().BeEquivalentTo(_user);
     }
+
+    [Fact]
+    public void DeleteUser_ExistingUserId_DeletesUser()
+    {
+        // Arrange
+        var dbService = new DbService(_context);
+        _context.Users.Add(_user);
+        _context.SaveChanges();
+        var userId = _context.Users.First().Id;
+
+        // Act
+        dbService.DeleteUser(userId);
+
+        // Assert
+        var user = _context.Users.SingleOrDefault(u => u.Id == userId);
+        user.Should().BeNull();
+    }
+
 }
