029f815321a76bf45ba5c6dea7539024c07c4d61
Caglar Özdemir <c.oezdemir@oeeng.de>
2025-01-14 11:52:28 +0100

    add Area Calculation tests

--- Geometries.Tests/MainGeometrieTests.cs
+++ Geometries.Tests/MainGeometrieTests.cs
@@ -29,5 +29,29 @@ namespace Geometries.Tests
             // Assert
             Assert.Equal(expectedLength, length);
         }
+
+        [Theory]
+        [InlineData(1, 10)]
+        [InlineData(5, 50)]
+        [InlineData(10, 100)]
+        public void GetArea_MockGeometrie_CorrectArea(int numberOfGeometries, int expectedArea)
+        {
+            // Arrange 
+            var mockGeometrie = Substitute.For<IGeometry>();
+            mockGeometrie.GetArea().Returns(10);
+            var list = new List<IGeometry>();
+            for (int i = 0; i < numberOfGeometries; i++)
+            {
+                list.Add(mockGeometrie);
+            }
+
+            var mainGeometrie = new MainGeometry(list);
+
+            // Act
+            var area = mainGeometrie.GetArea();
+
+            // Assert
+            Assert.Equal(expectedArea, area);
+        }
     }
 }
